45317bc83827f58ab7a5d2bd754f616033a59ab9
Darwin Libioulle <darwin.libioulle@erasme.ulb.ac.be>
2020-05-25 17:31:00 +0200

    Authentification Start

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,6 +22,7 @@ from boards import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     #path('privacy/',views.privacy_policy, name='privacy_policy'),
     path('',views.home,name='home'),
 ]
