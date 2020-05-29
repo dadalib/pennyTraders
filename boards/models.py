7dc375bfc5c48ef9e0f92cb1894b8ac6d4e2fe23
Darwin Libioulle <darwin.libioulle@erasme.ulb.ac.be>
2020-05-29 11:44:51 +0200

    QuerySets

--- boards/models.py
+++ boards/models.py
@@ -16,6 +16,9 @@ class Topic(models.Model):
     starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
     last_updated = models.DateTimeField(auto_now_add = True)
 
+    def __str__(self):
+        return self.subject
+
 class Post(models.Model):
     message = models.TextField(max_length = 4000)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name ='posts')
@@ -23,9 +26,13 @@ class Post(models.Model):
     update_at = models.DateTimeField(null =True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'posts')
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name ='+')
-    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name ='+')
+    #updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name ='+')
     #updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name ='+')
     #updated_by = models.ForeignKey(User,null=True, related_name ='+')
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
 
 
 
